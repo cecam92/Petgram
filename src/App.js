ab82850fb83602ce7b3619a79184ce9355617b7f
Cesar Martinez <cecam92@hotmail.com>
2020-11-17 20:36:28 +1100

    fadeIn animation to PhotoCard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { GlobalStyle } from './GlobalStyle'
+import { GlobalStyle } from './components/styles/GlobalStyle'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 
 export default function () {
   return (
     <>
       <GlobalStyle />
+      <Logo />
       <ListOfCategories />
       <ListOfPhotoCards />
     </>

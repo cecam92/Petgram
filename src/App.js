78ca54dc75269213ea6b04f0be35d6072bbd9a72
Cesar Martinez <cecam92@hotmail.com>
2020-12-01 22:35:37 +1100

    Favbutton and like mutation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import { GlobalStyle } from './components/styles/GlobalStyle'
 import { ListOfCategories } from './components/ListOfCategories'
-import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { ListOfPhotoCards } from './container/ListOfPhotoCards'
 import { Logo } from './components/Logo'
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 
 export default function () {
+  const urlParamas = new window.URLSearchParams(window.location.search)
+  const detailId = urlParamas.get('detail')
   return (
     <>
       <GlobalStyle />
       <Logo />
-      <ListOfCategories />
-      <ListOfPhotoCards />
+      {detailId ? (
+        <PhotoCardWithQuery id={detailId} />
+      ) : (
+        <>
+          <ListOfCategories />
+          <ListOfPhotoCards />
+        </>
+      )}
     </>
   )
 }
